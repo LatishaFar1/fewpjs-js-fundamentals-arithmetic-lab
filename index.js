ebfd158c2f74f3b3b882846e656d0ffbb4d1841f
Latisha Faroul <Lfaroul14@gmail.com>
2021-05-19 16:53:57 -0400

    commit tree b622a259aa58a73297831744298ec2e95308447a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2, num2 = 31, num3 = 5, num4 = 64, range = (1,3,5,6,11,15,20);
+let multiply = num1 * num2;
+
+//Create a variable called random that will generate a random integer greater than 0.//
+let random = Math.floor(Math.random() * 10) +1;
+
+let mod = num4 % num3;
+
+let max = Math.max(range);
